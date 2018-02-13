4e6f9c6bc337fa5ac9ce3c22ab3378cf64d78893
lexusdrumgold <LexusDrumgold@gmail.com>
2018-02-13 18:40:15 -0500

    file structire

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+
+import App from './components/app';
+
+import './stylesheets/app.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
