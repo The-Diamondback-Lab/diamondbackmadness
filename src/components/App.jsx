b74e0edd4f6e58c3c48ee3d712c6199573c0aecc
Akshay Guthal <akguthal@gmail.com>
2018-03-26 14:38:58 -0400

    added google analytics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,16 +13,21 @@ import Vote from '../pages/Vote';
 import '../stylesheets/normalize.min.css';
 import '../stylesheets/app.min.css';
 
+//Google Analytics
+import Analytics from 'react-router-ga';
+
 export default class App extends Component {
 
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Header/>
-          <Route exact={true} path='/' component={Home}/>
-          <Route exact={true} path='/vote' component={Vote}/> {this.props.children}
-        </div>
+        <Analytics id="UA-116382668-1" debug>
+          <div>
+            <Header/>
+            <Route exact={true} path='/' component={Home}/>
+            <Route exact={true} path='/vote' component={Vote}/> {this.props.children}
+          </div>
+        </Analytics>
       </BrowserRouter>
     );
   }
